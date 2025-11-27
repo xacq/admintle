ade5d27865d9d305ea4187b67f7ede1f684bb311
xacq <tu-email@example.com>
2025-11-26 23:10:23 -0500

    updating seeders

--- backend/database/seeders/UsersSeeder.php
+++ backend/database/seeders/UsersSeeder.php
@@ -15,156 +15,156 @@ class UsersSeeder extends Seeder
                 'name' => 'Laura Tutor',
                 'email' => 'tutor@example.com',
                 'username' => 'tutor',
-                'foto' => null,
+                'email_verified_at' => null,
                 'password' => '$2y$12$BZ3r5lmMIe8OyOOD...sW9xIJvVEhRIuaIsbW',
                 'role_id' => 68,
-                'email_verified_at' => null,
+                'remember_token' => null,
                 'created_at' => '2025-11-13 03:16:07',
                 'updated_at' => '2025-11-13 03:16:07',
-                'estado' => 't'
+                'is_active' => 't'
             ],
             [
                 'id' => 138,
                 'name' => 'Bruno Becario',
                 'email' => 'becario@example.com',
                 'username' => 'becario',
-                'foto' => null,
+                'email_verified_at' => null,
                 'password' => '$2y$12$DL1a5SwZ3E...Mx6xPfAB9kxqgXpSsu',
                 'role_id' => 69,
-                'email_verified_at' => null,
+                'remember_token' => null,
                 'created_at' => '2025-11-13 03:16:07',
                 'updated_at' => '2025-11-13 03:16:07',
-                'estado' => 't'
+                'is_active' => 't'
             ],
             [
                 'id' => 139,
                 'name' => 'Ana Administradora',
                 'email' => 'admin@example.com',
                 'username' => 'admin',
-                'foto' => null,
+                'email_verified_at' => null,
                 'password' => '$2y$12$./H1pvJdf...4l1rggx6jnDYK21e8u',
                 'role_id' => 70,
-                'email_verified_at' => null,
+                'remember_token' => null,
                 'created_at' => '2025-11-13 03:16:07',
                 'updated_at' => '2025-11-13 03:16:07',
-                'estado' => 't'
+                'is_active' => 't'
             ],
             [
                 'id' => 140,
                 'name' => 'Diego Director',
                 'email' => 'director@example.com',
                 'username' => 'director',
-                'foto' => null,
+                'email_verified_at' => null,
                 'password' => '$2y$12$QeS.ZDW....0FpUu0YeERy2AKvDy',
                 'role_id' => 71,
-                'email_verified_at' => null,
+                'remember_token' => null,
                 'created_at' => '2025-11-13 03:16:07',
                 'updated_at' => '2025-11-13 03:16:07',
-                'estado' => 't'
+                'is_active' => 't'
             ],
             [
                 'id' => 141,
                 'name' => 'Ana Guzmán',
                 'email' => 'ana.guzman@example.com',
                 'username' => 'ana.guzman',
-                'foto' => null,
+                'email_verified_at' => null,
                 'password' => '$2y$12$omneI5r...I9B/WcKaitvV6gQe2m',
                 'role_id' => 72,
-                'email_verified_at' => null,
+                'remember_token' => null,
                 'created_at' => '2025-11-13 03:16:07',
                 'updated_at' => '2025-11-13 03:16:07',
-                'estado' => 't'
+                'is_active' => 't'
             ],
             [
                 'id' => 142,
                 'name' => 'Luis Mamani',
                 'email' => 'luis.mamani@example.com',
                 'username' => 'luis.mamani',
-                'foto' => null,
+                'email_verified_at' => null,
                 'password' => '$2y$12$akFD...2ornEu.hYdLERqU0oW',
                 'role_id' => 72,
-                'email_verified_at' => null,
+                'remember_token' => null,
                 'created_at' => '2025-11-13 03:16:07',
                 'updated_at' => '2025-11-13 03:16:07',
-                'estado' => 't'
+                'is_active' => 't'
             ],
             [
                 'id' => 143,
                 'name' => 'José Flores',
                 'email' => 'jose.flores@example.com',
                 'username' => 'jose.flores',
-                'foto' => null,
+                'email_verified_at' => null,
                 'password' => '$2y$12$WpWl...DDydhUS50cpfv41Jwu',
                 'role_id' => 72,
-                'email_verified_at' => null,
+                'remember_token' => null,
                 'created_at' => '2025-11-13 03:16:07',
                 'updated_at' => '2025-11-13 03:16:07',
-                'estado' => 't'
+                'is_active' => 't'
             ],
             [
                 'id' => 144,
                 'name' => 'Lic. Anny Mercado Algarañaz',
                 'email' => 'anny.mercado@example.com',
                 'username' => 'anny.mercado',
-                'foto' => null,
+                'email_verified_at' => null,
                 'password' => 'D119xpTSrH/dkPuhQq', // viene así en tu archivo
                 'role_id' => 68,
-                'email_verified_at' => null,
+                'remember_token' => null,
                 'created_at' => '2025-11-13 03:16:07',
                 'updated_at' => '2025-11-13 03:16:07',
-                'estado' => 't'
+                'is_active' => 't'
             ],
             [
                 'id' => 145,
                 'name' => 'Dr. Luis Rojas',
                 'email' => 'luis.rojas@example.com',
                 'username' => 'luis.rojas',
-                'foto' => null,
+                'email_verified_at' => null,
                 'password' => '$2y$12$DxZ...HMxpUzjdA2EYjwoT86',
                 'role_id' => 68,
-                'email_verified_at' => null,
+                'remember_token' => null,
                 'created_at' => '2025-11-13 03:16:07',
                 'updated_at' => '2025-11-13 03:16:07',
-                'estado' => 't'
+                'is_active' => 't'
             ],
             [
                 'id' => 146,
                 'name' => 'MSc. Juan García',
                 'email' => 'juan.garcia@example.com',
                 'username' => 'juan.garcia',
-                'foto' => null,
+                'email_verified_at' => null,
                 'password' => '$2y$12...jRCGrmu7jWZRITkXj.',
                 'role_id' => 68,
-                'email_verified_at' => null,
+                'remember_token' => null,
                 'created_at' => '2025-11-13 03:16:07',
                 'updated_at' => '2025-11-13 03:16:07',
-                'estado' => 't'
+                'is_active' => 't'
             ],
             [
                 'id' => 147,
                 'name' => 'tuto',
                 'email' => 'tuto@email.com',
                 'username' => 'tuto',
-                'foto' => null,
+                'email_verified_at' => null,
                 'password' => '$2y$12$MwfYxJtL93D3PW3MIZ8GE.nHFi8...6aYpfrkCfFKuBPrxgC',
                 'role_id' => 68,
-                'email_verified_at' => null,
+                'remember_token' => null,
                 'created_at' => '2025-11-13 03:19:11',
                 'updated_at' => '2025-11-13 03:19:11',
-                'estado' => 't'
+                'is_active' => 't'
             ],
             [
                 'id' => 148,
                 'name' => 'beca',
                 'email' => 'beca@correo.com',
                 'username' => 'beca',
-                'foto' => null,
+                'email_verified_at' => null,
                 'password' => '$2y$12$G7pHCypJJ/8oar7fGrWIeOT1Ta...P3YFbDROOrIt2uRAO6',
                 'role_id' => 69,
-                'email_verified_at' => null,
+                'remember_token' => null,
                 'created_at' => '2025-11-13 03:20:04',
                 'updated_at' => '2025-11-13 03:20:04',
-                'estado' => 't'
+                'is_active' => 't'
             ],
         ]);
     }
